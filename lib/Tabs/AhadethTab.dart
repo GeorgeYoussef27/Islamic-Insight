7fe264d6cf7daa4db30c86ba1f1710ebf72c0415
Geo <georgeyoussefrady@gmail.com>
2024-10-13 05:09:20 +0300

    +finish ahadeth details screen

--- lib/Tabs/AhadethTab.dart
+++ lib/Tabs/AhadethTab.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:islamy_app/ahadeth_details/ahadeth_details_screen.dart';
 import 'package:islamy_app/home/Hadeth.dart';
 
 class AhadethTab extends StatefulWidget {
-
   @override
   State<AhadethTab> createState() => _AhadethTabState();
 }
@@ -11,12 +11,12 @@ class AhadethTab extends StatefulWidget {
 class _AhadethTabState extends State<AhadethTab> {
   @override
   void initState() {
-     super.initState();
+    super.initState();
     loadFile();
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Column(
       children: [
         Expanded(child: Image.asset("assets/images/hadith_header.png")),
@@ -27,15 +27,23 @@ class _AhadethTabState extends State<AhadethTab> {
         Expanded(
           flex: 2,
           child: ListView.separated(
-              itemBuilder: (context, index) => Text(
-                    ahadethList[index].title,
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
-                    textAlign: TextAlign.center,
+              itemBuilder: (context, index) => InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(
+                          context, AhadethDetailsScreen.routeName,
+                          arguments: ahadethList[index]);
+                    },
+                    child: Text(
+                      ahadethList[index].title,
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+                      textAlign: TextAlign.center,
+                    ),
                   ),
               separatorBuilder: (context, index) => SizedBox(
                     height: 10,
                   ),
-              itemCount: ahadethList.length ),
+              itemCount: ahadethList.length),
         )
       ],
     );
@@ -43,18 +51,17 @@ class _AhadethTabState extends State<AhadethTab> {
 
   List<Hadeth> ahadethList = [];
 
-  loadFile()async{
-    String fileContent = await rootBundle.loadString("assets/files/ahadeth.txt");
+  loadFile() async {
+    String fileContent =
+        await rootBundle.loadString("assets/files/ahadeth.txt");
     List<String> ahadeth = fileContent.split("#");
-    for(int i=0; i<ahadeth.length; i++){
+    for (int i = 0; i < ahadeth.length; i++) {
       List<String> hadethLines = ahadeth[i].trim().split("\n");
       String hadethTitle = hadethLines[0];
       hadethLines.removeAt(0);
       String hadethContent = hadethLines.join(" ");
       ahadethList.add(Hadeth(hadethTitle, hadethContent));
     }
-    setState(() {
-
-    });
+    setState(() {});
   }
 }
